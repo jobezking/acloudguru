7b7c9ebaf47629ce0f83f33dea6650cc601527d9
Job King <jobezking@gmail.com>
2022-09-18 20:40:29 -0400

    add chrome remote desktop install script

--- main.tf
+++ main.tf
@@ -10,13 +10,19 @@ resource "google_compute_instance" "vm_instance" {
   }
   network_interface {
     network = google_compute_network.vpc_network.name
+    access_config {
+      nat_ip = ""
+    }
   }
+  allow_stopping_for_update = true
+  metadata_startup_script   = file("./src/startup.sh")
 }
 
 
 resource "google_compute_instance" "vm_instance-micro" {
   name         = "terraform-instance-micro"
   machine_type = "f1-micro"
+  tags         = ["web"]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -26,14 +32,17 @@ resource "google_compute_instance" "vm_instance-micro" {
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
-
+      nat_ip = ""
     }
   }
+  allow_stopping_for_update = true
+  metadata_startup_script   = file("./src/install-chrome-remote-desktop.sh")
 }
 
 resource "google_compute_instance" "vm_instance-module" {
   name         = "terraform-instance-module"
   machine_type = "f1-micro"
+  tags         = ["web"]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -44,8 +53,9 @@ resource "google_compute_instance" "vm_instance-module" {
     #network = var.module_network
     network = "default"
     access_config {
-
+      nat_ip = ""
     }
   }
   allow_stopping_for_update = true
+  metadata_startup_script   = file("./src/startup.sh")
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "ip" {
   value = google_compute_address.vm_static_ip.address
 }
+
+output "vm_instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+
+output "vm_instance-micro_ip" {
+  value = google_compute_instance.vm_instance-micro.network_interface.0.network_ip
+}
+
+output "vm_instance-module" {
+  value = google_compute_instance.vm_instance-module.network_interface.0.network_ip
+}
